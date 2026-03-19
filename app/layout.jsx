63ba7b8e9072044007b4e27c2109d5899f5755fb
.Astris <astr3on@gmail.com>
2026-03-19 12:57:36 +0700

    Format layout component for better readability

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,13 @@
 import "./globals.css";
 
 export const metadata = {
-    title: "SoftPatch",
+  title: "SoftPatch",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
